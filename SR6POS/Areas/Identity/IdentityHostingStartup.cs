26df27400d2450779e0e2f51dbc98f1e171802f9
samoeun chhay <samoeunchh@gmail.com>
2021-10-20 11:08:27 +0700

    Add project files.

---
+++ SR6POS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SR6POS.Data;
+
+[assembly: HostingStartup(typeof(SR6POS.Areas.Identity.IdentityHostingStartup))]
+namespace SR6POS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ SR6POS/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SR6POS.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SR6POS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
