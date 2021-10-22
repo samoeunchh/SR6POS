0a2f0b901f4a5ea6029d9664c78207c3a3b1194e
samoeun chhay <samoeunchh@gmail.com>
2021-10-22 07:26:58 +0700

    Add Unit

--- SR6POS/Data/ApplicationDbContext.cs
+++ SR6POS/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace SR6POS.Data
         {
         }
         public DbSet<Category> Category { get; set; }
+        public DbSet<Unit> Unit { get; set; }
     }
 }
---
+++ SR6POS/Models/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SR6POS.Models
+{
+    public class Unit
+    {
+        [Key]
+        public Guid UnitId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name ="Unit Name")]
+        public string UnitName { get; set; }
+        public int Qty { get; set; }
+    }
+}
