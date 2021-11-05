f63335153d6ff5c2ca4069cdf26cd8290e2e879a
samoeun chhay <samoeunchh@gmail.com>
2021-11-05 08:13:07 +0700

    Design Sale Layout

---
+++ SR6POS/Models/Sale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SR6POS.Models
+{
+    public class Sale
+    {
+        [Key]
+        public Guid SaleId { get; set; }
+        [ForeignKey("Customer")]
+        [Display(Name ="Customer Name")]
+        public Guid CustomerId { get; set; }
+        [MaxLength(15)]
+        public string InvoiceNumber { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime IssueDate { get; set; }
+        public double Total { get; set; }
+        public double Discount { get; set; }
+        public double GrandTotal { get; set; }
+        public double Receive { get; set; }
+        public double Change { get; set; }
+
+        public Customer Customer { get; set; }
+        public List<SaleDetail> SaleDetails { get; set; }
+    }
+}
