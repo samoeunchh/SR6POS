11032df793268d733ad79ee9045368193e7458a0
samoeun chhay <samoeunchh@gmail.com>
2021-11-10 11:12:45 +0700

    Scan item and calc on Sale

---
+++ SR6POS/Controllers/BarcodeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SR6POS.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SR6POS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarcodeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public BarcodeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{code}")]
+        public async Task<IActionResult> Get(string code)
+        {
+            var product = await (from p in _context.Product
+                                 join pp in _context.ProductPrice on p.SaleUnit equals pp.UnitId
+                                 where p.Barcode.Equals(code)
+                                 select new
+                                 {
+                                     p.ProductId,
+                                     p.ProductName,
+                                     p.Barcode,
+                                     p.OnHand,
+                                     p.Cost,
+                                     pp.UnitId,
+                                     pp.Price
+                                 }).FirstOrDefaultAsync();
+            return Ok(product);
+        }
+    }
+}
--- SR6POS/wwwroot/js/site.js
+++ SR6POS/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var options = "";
+
+$.ajax({
+    method: "GET",
+    url: "/Products/GetUnits",
+    contentType: "application/json",
+    success: function (data) {
+        $.each(data, function (i, v) {
+            options += "<option value='" + v.unitId + "'>" + v.unitName + "</option>";
+        });
+    },
+    error: function (error) {
+        console.log(error);
+    }
+});
